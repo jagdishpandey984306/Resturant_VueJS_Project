ac20e7be8d5b079034bbfa4f4536e0e91b6c2360
aayushdura <aayushdura86@gmail.com>
2023-07-24 13:08:41 +0545

    fixed babel issue and added toast example in Login component

--- resto-project/src/main.js
+++ resto-project/src/main.js
@@ -1,16 +1,17 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import ValidationPlugin from './validation.js';
-import fontAwesomePlugin from './fontAwesomePlugin';
-import apiUrlPlugin from './api'
-import toastifyPlugin from './toastifyPlugin';
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "bootstrap/dist/css/bootstrap.min.css";
+import ValidationPlugin from "./validation.js";
+import fontAwesomePlugin from "./fontAwesomePlugin";
+import apiUrlPlugin from "./api";
+import { Toast } from "vue-toastr";
+// import toastifyPlugin from './toastifyPlugin';
 
 createApp(App)
-    .use(router)
-    .use(ValidationPlugin)
-    .use(fontAwesomePlugin)
-    .use(toastifyPlugin)
-    .mixin(apiUrlPlugin)
-    .mount('#app')
+  .use(router)
+  .use(ValidationPlugin)
+  .use(fontAwesomePlugin)
+  .use(Toast)
+  .mixin(apiUrlPlugin)
+  .mount("#app");
--- resto-project/src/toastifyPlugin.js
+++ resto-project/src/toastifyPlugin.js
@@ -1,26 +1,23 @@
-import { createApp } from 'vue';
-import Toastify from 'vue3-toastify';
+// export default {
+//   install(app) {
+//     // Set your default options here
+//     const defaultOptions = {
+//       position: "bottom-right",
+//       timeout: 3000,
+//       closeOnClick: true,
+//       pauseOnFocusLoss: true,
+//       pauseOnHover: true,
+//       draggable: true,
+//       showCloseButtonOnHover: false,
+//     };
 
-export default {
-    install(app) {
-        // Set your default options here
-        const defaultOptions = {
-            position: 'bottom-right',
-            timeout: 3000,
-            closeOnClick: true,
-            pauseOnFocusLoss: true,
-            pauseOnHover: true,
-            draggable: true,
-            showCloseButtonOnHover: false,
-        };
+//     // Merge the default options with the user-defined options (if any)
+//     const toastifyOptions = {
+//       ...defaultOptions,
+//       // Add any other custom options if needed
+//     };
 
-        // Merge the default options with the user-defined options (if any)
-        const toastifyOptions = {
-            ...defaultOptions,
-            // Add any other custom options if needed
-        };
-
-        // Register the plugin with the default options
-        app.use(Toastify, toastifyOptions);
-    },
-};
+//     // Register the plugin with the default options
+//     app.use(Toastify, toastifyOptions);
+//   },
+// };
